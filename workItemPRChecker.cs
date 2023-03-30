eea50849decb173ddd8f7001b278f40cdf38beec
frankchen <frankchen93011@gmail.com>
2023-03-31 00:05:55 +0800

    add commit query

--- workItemPRChecker.cs
+++ workItemPRChecker.cs
@@ -9,6 +9,9 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs.Host;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
 
 public static class WorkItemCommitDifferenceFunction
 {
@@ -16,6 +19,10 @@ public static class WorkItemCommitDifferenceFunction
     public static async Task<object> Run(
         [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, TraceWriter log)
     {
+        string PAT = Environment.GetEnvironmentVariable("PAT", EnvironmentVariableTarget.Process);
+        log.Info("TEST: " + PAT);
+
+
         string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
         dynamic data = JsonConvert.DeserializeObject(requestBody);
         log.Info("Data Received: " + data.ToString());
@@ -31,12 +38,7 @@ public static class WorkItemCommitDifferenceFunction
         string branchName = data.resource.sourceRefName;
         string baseBranchName = "test";
 
-        var commitClient = new HttpClient();
-        var commitRequest = new HttpRequestMessage(HttpMethod.Get, $"{pullRequestUrl}/commits?api-version=7.0");
-        HttpResponseMessage commitResponse = await commitClient.SendAsync(commitRequest);
-        string commitResponseContent = await commitResponse.Content.ReadAsStringAsync();
-        log.Info("commits response: " + commitResponseContent);
-        dynamic commits = JsonConvert.DeserializeObject(commitResponseContent);
+        var commits = await GetPullRequestCommits(pullRequestUrl, PAT);
         log.Info("commits Received: " + commits.ToString());
 
         // var workItemClient = new HttpClient();
@@ -48,5 +50,21 @@ public static class WorkItemCommitDifferenceFunction
 
 
         return new OkObjectResult($"There is no difference between {branchName} and {baseBranchName} in the work item commit.");
+    }
+
+    private static async Task<dynamic> GetPullRequestCommits(string pullRequestUrl, string accessToken)
+    {
+        using var client = new HttpClient();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(
+                ASCIIEncoding.ASCII.GetBytes(
+                string.Format("{0}:{1}", "", accessToken))));
+
+        var response = await client.GetAsync($"{pullRequestUrl}/commits?api-version=6.0");
+        response.EnsureSuccessStatusCode();
+        var responseBody = await response.Content.ReadAsStringAsync();
+        var commits = JsonConvert.DeserializeObject<dynamic>(responseBody);
+
+        return commits;
     }
 }
