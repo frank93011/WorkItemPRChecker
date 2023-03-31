cd2afc82b840ddd9972da9e7d8f207166bb4449a
frankchen <frankchen93011@gmail.com>
2023-03-31 18:27:52 +0800

    query workItems related commits

--- workItemPRChecker.cs
+++ workItemPRChecker.cs
@@ -13,6 +13,45 @@ using System.Collections.Generic;
 using System.Net.Http.Headers;
 using System.Text;
 
+
+public class CommitResponse
+{
+    public int count { get; set; }
+    public List<Commit> value { get; set; }
+}
+
+public class Commit
+{
+    public string commitId { get; set; }
+    public string comment { get; set; }
+    public Author author { get; set; }
+    public string url { get; set; }
+}
+
+public class Author
+{
+    public string name { get; set; }
+    public string email { get; set; }
+    public string date { get; set; }
+}
+
+public class WorkItemResponse
+{
+    public int count { get; set; }
+    public List<WorkItem> value { get; set; }
+}
+
+public class WorkItem
+{
+    public string id { get; set; }
+    public string url { get; set; }
+}
+
+public class WorkItemRelationResponse
+{
+    public List<dynamic> relations { get; set; }
+}
+
 public static class WorkItemCommitDifferenceFunction
 {
     [FunctionName("WorkItemCommitDifferenceFunction")]
@@ -20,12 +59,10 @@ public static class WorkItemCommitDifferenceFunction
         [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, TraceWriter log)
     {
         string PAT = System.Environment.GetEnvironmentVariable("PAT", EnvironmentVariableTarget.Process);
-        log.Info("TEST: " + PAT);
-
 
         string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
         dynamic data = JsonConvert.DeserializeObject(requestBody);
-        log.Info("Data Received: " + data.ToString());
+        // log.Info("Data Received: " + data.ToString());
 
         string eventType = req.Headers["X-GitHub-Event"];
         // if (eventType != "pull_request")
@@ -35,24 +72,39 @@ public static class WorkItemCommitDifferenceFunction
 
         string repositoryName = data.resource.repository.name;
         string pullRequestUrl = data.resource.url;
+        string pullRequestId = data.resource.pullRequestId;
         string branchName = data.resource.sourceRefName;
-        string baseBranchName = "test";
+        string repositoryUrl = data.resource.repository.url;
+        string repoId = data.resource.repository.id;
+        string projectId = data.resource.repository.project.id;
+        string organization = repositoryUrl.Split('/')[3];
 
-        var commits = await GetPullRequestCommits(pullRequestUrl, PAT);
-        log.Info("commits Received: " + commits.ToString());
+        // var commits = await GetCommitsFromPR(org, , PAT);
+        // log.Info("commits Received: " + commits.ToString());
 
-        // var workItemClient = new HttpClient();
-        // var workItemRequest = new HttpRequestMessage(HttpMethod.Get, $"https://dev.azure.com/{repositoryName}/_apis/wit/workitems?ids={string.Join(',', commits.Select(c => c.id.ToString()))}&api-version=6.1-preview.3");
-        // HttpResponseMessage workItemResponse = await workItemClient.SendAsync(workItemRequest);
-        // string workItemResponseContent = await workItemResponse.Content.ReadAsStringAsync();
-        // dynamic workItems = JsonConvert.DeserializeObject(workItemResponseContent);
-        // log.Info("workItems Received: " + workItems.ToString());
+        var workItems = await GetWorkItemsFromPR(pullRequestUrl, PAT);
+        log.Info("workItems Received: " + workItems.ToString());
 
+        var prIds = await GetPrIdsFromWorkItems(workItems, organization, projectId, PAT);
+        log.Info("PRs Received: " + prIds.ToString());
 
-        return new OkObjectResult($"There is no difference between {branchName} and {baseBranchName} in the work item commit.");
+        HashSet<string> relatedCommits = new HashSet<string>();
+        string responseMessage = "";
+        foreach (var prId in prIds)
+        {
+            var commits = await GetCommitsFromPR(organization, projectId, repoId, prId, PAT);
+            commits.ForEach(commit =>
+            {
+                relatedCommits.Add(commit.commitId);
+                responseMessage += commit.commitId + '\n';
+            });
+        }
+
+        return new OkObjectResult($"{responseMessage}");
     }
 
-    private static async Task<dynamic> GetPullRequestCommits(string pullRequestUrl, string accessToken)
+
+    private static async Task<string> GetResponseStringFromClient(string url, string accessToken)
     {
         using var client = new HttpClient();
         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
@@ -60,11 +112,46 @@ public static class WorkItemCommitDifferenceFunction
                 ASCIIEncoding.ASCII.GetBytes(
                 string.Format("{0}:{1}", "", accessToken))));
 
-        var response = await client.GetAsync($"{pullRequestUrl}/commits?api-version=6.0");
+        var response = await client.GetAsync(url);
         response.EnsureSuccessStatusCode();
-        var responseBody = await response.Content.ReadAsStringAsync();
-        var commits = JsonConvert.DeserializeObject<dynamic>(responseBody);
+        string responseBody = await response.Content.ReadAsStringAsync();
+        return responseBody;
+    }
 
-        return commits;
+    private static async Task<List<Commit>> GetCommitsFromPR(string org, string projectId, string repoId, string prId, string accessToken)
+    {
+        string targetUrl = $"https://dev.azure.com/{org}/{projectId}/_apis/git/repositories/{repoId}/pullRequests/{prId}/commits?api-version=7.0";
+        string responseBody = await GetResponseStringFromClient(targetUrl, accessToken);
+        var commitResponse = JsonConvert.DeserializeObject<CommitResponse>(responseBody);
+
+        return commitResponse.value;
+    }
+
+    private static async Task<List<WorkItem>> GetWorkItemsFromPR(string pullRequestUrl, string accessToken)
+    {
+        string targetUrl = $"{pullRequestUrl}/workitems?api-version=7.0";
+        string responseBody = await GetResponseStringFromClient(targetUrl, accessToken);
+        var workItems = JsonConvert.DeserializeObject<WorkItemResponse>(responseBody);
+        return workItems.value;
+    }
+
+    private static async Task<List<string>> GetPrIdsFromWorkItems(List<WorkItem> workItems, string org, string projectId, string accessToken)
+    {
+        List<string> pullRequestIds = new List<string>();
+        foreach (WorkItem item in workItems)
+        {
+            string targetUrl = $"https://dev.azure.com/{org}/{projectId}/_apis/wit/workitems/{item.id}?$expand=relations";
+            string responseBody = await GetResponseStringFromClient(targetUrl, accessToken);
+            dynamic prs = JsonConvert.DeserializeObject(responseBody);
+            foreach (var pr in prs.relations)
+            {
+                if (pr.attributes.name != "Pull Request") continue;
+                string url = pr.url;
+                string id = url.Split("%2F").Last();
+                pullRequestIds.Add(id);
+            }
+        }
+
+        return pullRequestIds;
     }
 }
