6b1057bc6861b32643474b16387c1234787d3aeb
frankchen <frankchen93011@gmail.com>
2023-04-01 00:09:49 +0800

    add status update

--- workItemPRChecker.cs
+++ workItemPRChecker.cs
@@ -12,7 +12,7 @@ using Microsoft.Azure.WebJobs.Host;
 using System.Collections.Generic;
 using System.Net.Http.Headers;
 using System.Text;
-
+using System.Net;
 
 public class CommitResponse
 {
@@ -58,44 +58,59 @@ public static class WorkItemCommitDifferenceFunction
     public static async Task<object> Run(
         [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, TraceWriter log)
     {
-        string PAT = System.Environment.GetEnvironmentVariable("PAT", EnvironmentVariableTarget.Process);
-
-        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-        dynamic data = JsonConvert.DeserializeObject(requestBody);
-
-        string eventType = req.Headers["X-GitHub-Event"];
-        // if (eventType != "pull_request")
-        // {
-        //     return new OkObjectResult("Not a pull request event.");
-        // }
-
-        string repositoryName = data.resource.repository.name;
-        string pullRequestUrl = data.resource.url;
-        string currentPrId = data.resource.pullRequestId;
-        string sourceRefName = data.resource.sourceRefName;
-        string repositoryUrl = data.resource.repository.url;
-        string repoId = data.resource.repository.id;
-        string projectId = data.resource.repository.project.id;
-        string organization = repositoryUrl.Split('/')[3];
-        string currentBranch = sourceRefName.Split('/')[2];
-
-        var workItems = await GetWorkItemsFromPR(pullRequestUrl, PAT);
-        log.Info("workItems Received: " + workItems.ToString());
-
-        var prIds = await GetPrIdsFromWorkItems(workItems, organization, projectId, PAT);
-        log.Info("PRs Received: " + prIds.ToString());
-
-        List<string> relatedCommits = await GetRelatedCommitsFromPRs(prIds, currentPrId, organization, projectId, repoId, PAT);
-
-        string[] targetBranchs = { "main", "SIT", "PROD" };
-        string responseMessage = "";
-        foreach (string targetBranch in targetBranchs)
+        try
         {
-            responseMessage += await CompareCommitsDiffWithTargetBranch(relatedCommits, organization, projectId, repoId, targetBranch, currentBranch, PAT);
-            responseMessage += "--------------\n";
-        }
+            string PAT = System.Environment.GetEnvironmentVariable("PAT", EnvironmentVariableTarget.Process);
 
-        return new OkObjectResult($"{responseMessage}");
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            string eventType = req.Headers["X-GitHub-Event"];
+            // if (eventType != "pull_request")
+            // {
+            //     return new OkObjectResult("Not a pull request event.");
+            // }
+
+            string repositoryName = data.resource.repository.name;
+            string pullRequestUrl = data.resource.url;
+            string currentPrId = data.resource.pullRequestId;
+            string sourceRefName = data.resource.sourceRefName;
+            string repositoryUrl = data.resource.repository.url;
+            string repoId = data.resource.repository.id;
+            string projectId = data.resource.repository.project.id;
+            string organization = repositoryUrl.Split('/')[3];
+            string currentBranch = sourceRefName.Split('/')[2];
+
+            var workItems = await GetWorkItemsFromPR(pullRequestUrl, PAT);
+            log.Info("workItems Received: " + workItems.ToString());
+
+            var prIds = await GetPrIdsFromWorkItems(workItems, organization, projectId, PAT);
+            log.Info("PRs Received: " + prIds.ToString());
+
+            List<string> relatedCommits = await GetRelatedCommitsFromPRs(prIds, currentPrId, organization, projectId, repoId, PAT);
+
+            string[] targetBranchs = { "main", "SIT", "PROD" };
+            string responseMessage = "";
+            bool hasDiff = false;
+            foreach (string targetBranch in targetBranchs)
+            {
+                var res = await CompareCommitsDiffWithTargetBranch(relatedCommits, organization, projectId, repoId, targetBranch, currentBranch, PAT);
+                hasDiff |= res.Item1;
+                responseMessage += res.Item2;
+                responseMessage += "--------------\n";
+            }
+
+            log.Info($"{responseMessage}");
+
+            PostStatusOnPullRequest(organization, projectId, repoId, currentPrId, ComputeStatus(hasDiff, responseMessage), PAT);
+
+            return new OkObjectResult($"{responseMessage}");
+        }
+        catch (Exception ex)
+        {
+            log.Error(ex.ToString());
+            return new ObjectResult(HttpStatusCode.InternalServerError + ex.ToString());
+        }
     }
 
 
@@ -165,7 +180,7 @@ public static class WorkItemCommitDifferenceFunction
         return pullRequestIds;
     }
 
-    private static async Task<string> CompareCommitsDiffWithTargetBranch(List<string> currentCommits, string org, string projectId, string repoId, string targetBranch, string currentBranch, string accessToken)
+    private static async Task<Tuple<bool, string>> CompareCommitsDiffWithTargetBranch(List<string> currentCommits, string org, string projectId, string repoId, string targetBranch, string currentBranch, string accessToken)
     {
         HashSet<string> branchTotalCommits = new HashSet<string>();
         string commitDiffMessege = $"[{targetBranch}] branch missing the following commits from linked workItems:\n";
@@ -180,6 +195,50 @@ public static class WorkItemCommitDifferenceFunction
             hasDiff = true;
             commitDiffMessege += commitId + "\n";
         }
-        return hasDiff ? commitDiffMessege : $"[{targetBranch}] branch has no linked workItem related commits difference.\n";
+        string message = hasDiff ? commitDiffMessege : $"[{targetBranch}] branch has no linked workItem related commits difference.\n";
+        return new Tuple<bool, string>(hasDiff, message);
+    }
+
+    private static string ComputeStatus(bool hasDiff, string responseMessage)
+    {
+        string state = !hasDiff ? "succeeded" : "pending";
+
+        return JsonConvert.SerializeObject(
+            new
+            {
+                State = state,
+                Description = responseMessage,
+                TargetUrl = "https://visualstudio.microsoft.com",
+
+                Context = new
+                {
+                    Name = "PullRequest-workItemPRChecker",
+                    Genre = "pr-azure-function-ci"
+                }
+            });
+    }
+
+    private static void PostStatusOnPullRequest(string org, string projectId, string repoId, string currentPrId, string status, string accessToken)
+    {
+        string targetUrl = $"https://dev.azure.com/{org}/{projectId}/_apis/git/repositories/{repoId}/pullrequests/{currentPrId}/statuses?api-version=4.1";
+
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(
+                    ASCIIEncoding.ASCII.GetBytes(
+                    string.Format("{0}:{1}", "", accessToken))));
+
+            var method = new HttpMethod("POST");
+            var request = new HttpRequestMessage(method, targetUrl)
+            {
+                Content = new StringContent(status, Encoding.UTF8, "application/json")
+            };
+
+            using (HttpResponseMessage response = client.SendAsync(request).Result)
+            {
+                response.EnsureSuccessStatusCode();
+            }
+        }
     }
 }
